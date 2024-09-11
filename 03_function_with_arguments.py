cda0b1f468914fcf2e722203325b805d6fbf58e5
zakir <azizizakir449@gmail.com>
2024-09-11 20:13:07 +0500

    type of function

---
+++ 03_function_with_arguments.py
@@ -0,0 +1,7 @@
+def goodDay(name, ending):
+    print("Good Day, " + name)
+    print(ending)
+    return "ok"
+
+a = goodDay("Zakir", "thank you")
+print(a)
---
+++ 04_default_with_argument.py
@@ -0,0 +1,6 @@
+def goodDay(name, ending="thank you"):
+    print(f"Good Day, {name}")
+    print(ending)
+
+goodDay("Zakir", "Thanks")
+goodDay("Rohan")
