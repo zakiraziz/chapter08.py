177212bbcaf129b3177921f220db9da67e7f810d
zakir <azizizakir449@gmail.com>
2024-09-11 16:21:27 +0500

    fundtion

---
+++ 01_functions.py
@@ -0,0 +1,34 @@
+#a = int(input("Enetr your number"))
+#b = int(input("Enetr your number"))
+#c = int(input("Enetr your number"))
+
+#average = (a + b + c)/3
+#print(average)
+
+#a = int(input("Enetr your number"))
+#b = int(input("Enetr your number"))
+#c = int(input("Enetr your number"))
+
+#average = (a + b + c)/3
+#print(average)
+
+# Function Definition 
+def  avg():
+      a = int(input("Enetr your number"))
+      b = int(input("Enetr your number"))
+      c = int(input("Enetr your number"))
+
+      average = (a + b + c)/3
+      print(average)
+
+
+avg() # Function Call
+print("thank you")
+avg()
+print("thank you")
+avg()
+print("thank you")
+avg()
+print("thank you")
+avg()
+print("thank you")
